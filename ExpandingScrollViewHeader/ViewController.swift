6b170899c4d683480bf267108e50d4ed945a6c75
Yuichi Fujiki <fjk89025@gmail.com>
2019-06-10 16:50:45 +0800

    Adjust content inset of bottom.

--- ExpandingScrollViewHeader/ViewController.swift
+++ ExpandingScrollViewHeader/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
+        scrollView.contentInset.bottom = view.safeAreaInsets.bottom
+    }
 }
 
